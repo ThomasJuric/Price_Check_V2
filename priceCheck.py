6e26e1dae3a1ab62c946d0759421cb23dd824b57
ThomasJuric <thomasjuric@gmail.com>
2020-06-02 00:07:23 -0400

    Added eBay information to application. Now, Amazon and eBay are searched

--- priceCheck.py
+++ priceCheck.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-
+import re
+# ebay = requests.get('https://www.ebay.com/')
+# kijiji = requests.get('https://www.kijiji.ca/')
+# walmart = requests.get('https://www.walmart.com/')
+# facebook = requests.get('https://www.facebook.com/marketplace/')
 
 def searchAmazon(product):
     # Check the first 3 products
@@ -17,51 +21,101 @@ def searchAmazon(product):
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     }
     cookies = dict(cookies_are='working')
-    print ('searching for ' + product)
+    print ('Searching for product ' + product + ' on Amazon')
     amazon = requests.get(amazonLink, headers=headers)
     soup = BeautifulSoup(amazon.text, 'html.parser')
-    # ideas = soup.find('div', {'class':'sg-col-20-of-24 sg-col-28-of-32 sg-col-16-of-20 sg-col sg-col-32-of-36 sg-col-8-of-12 sg-col-12-of-16 sg-col-24-of-28'})
-    
-    # print(ideas)
-    # print(soup)
-    # data = soup.find_all('div')
     data = []
     for span in soup.find_all('span', {'class','a-price-whole'}):
         values = [span.text for div in span.find_all('span')]
         data.append(values)
+    links = []
+    for link in soup.find_all('a', {'class','a-text-normal'}):
+        links.append(link['href'])
     if(len(data) > 0):
-        return (data)
+        return (data, links)
     else:
         return(0)
 
-def getAmazonInformation(theData):
+def getAmazonInformation(theData, links):
     # Obtain the 3 top prices, as these are most likely the most related products to the user search
     if(len(theData) > 4):
+        price1 = str(theData[0])
+        price2 = str(theData[1])
+        price3 = str(theData[2])
+        price4 = str(theData[3])
         n = 4
         for i in range(n-1):
             for j in range(0, n-i-1):
                 if(theData[j] > theData[j+1]):
                     theData[j], theData[j+1] = theData[j+1], theData[j]
-
-    for i in range(4):
-        print ("%s" %str(theData[i]))
-    
-    print("Cheapest Price Found For Product Searched On Amazon Is: $" + str(theData[0]))
-
-    # print("Price 1 : " + price1)
-    # print("Price 2 : " + price2)
-    # print("Price 3 : " + price3)
-    # if(price1>price3):
-    #     print(price1 + " > " + price3)
-    # else:
-    #     print(price1 + " < " + price3)
+    theLink = ""
+    if(str(theData[0]) == price1):
+        theLink = str(links[0])
+    elif(str(theData[0]) == price2):
+        theLink = str(links[2])
+    elif(str(theData[0]) == price3):
+        theLink = str(links[4])
+    elif(str(theData[0]) == price4):
+        theLink = str(links[6])
+    theActualLink = 'amazon.ca' + theLink
+    return (theActualLink, str(theData[0]))
 
 
 
 
+def searchEbay(product):
+    ebayLink = ('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1312.R1.TR11.TRC2.A0.H0.Xla.TRS1&_nkw=' + product + '&_sacat=0')
+    headers = {
+        'authority': 'www.amazon.com',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
+    cookies = dict(cookies_are='working')
+    print ('Searching for product ' + product + ' on eBay')
+    print('Please note ** eBay is constantly being updated and results may not be the 100% consistent **')
+    ebay = requests.get(ebayLink, headers=headers)
+    soup = BeautifulSoup(ebay.text, 'html.parser')
+    data = []
+    for span in soup.find_all('span', {'class','s-item__price'}):
+        values = [span.text for div in span.find_all('span')]
+        data.append(values)
+    # print(data)
+    links = []
+    for link in soup.find_all('a', {'class','s-item__link'}):
+        links.append(link['href'])
+    # print(links)
+    if(len(data) > 0):
+        return (data, links)
+    else:
+        return(0)
 
-# def searchEbay(product):
-#     webbrowser.open(ebay, new=1)
+def getEbayInformation(theData, links):
+    if(len(theData) > 4):
+        price1 = str(theData[0])
+        price2 = str(theData[1])
+        price3 = str(theData[2])
+        price4 = str(theData[3])
+        n = 4
+        for i in range(n-1):
+            for j in range(0, n-i-1):
+                if(theData[j] > theData[j+1]):
+                    theData[j], theData[j+1] = theData[j+1], theData[j]
+    theLink = ""
+    if(str(theData[0]) == price1):
+        theLink = str(links[0])
+    elif(str(theData[0]) == price2):
+        theLink = str(links[1])
+    elif(str(theData[0]) == price3):
+        theLink = str(links[2])
+    elif(str(theData[0]) == price4):
+        theLink = str(links[3])
+    theActualLink = theLink
+    return (theActualLink, str(theData[0]))
 
 
 # def searchKijiji(product):
@@ -78,28 +132,50 @@ def getAmazonInformation(theData):
 
 
 product = input("Enter a product you'd like to search for: ")
-# ebay = requests.get('https://www.ebay.com/')
-# kijiji = requests.get('https://www.kijiji.ca/')
-# walmart = requests.get('https://www.walmart.com/')
-# facebook = requests.get('https://www.facebook.com/marketplace/')
-# print(amazon.headers)
-# print(soup)
+
+
+
+
+
 amazonCount = 0
 while ((amazonCount < 51)):
-    result = searchAmazon(product)
+    result, links = searchAmazon(product)
     if(result == 0):
         amazonCount = amazonCount + 1
         print('attempted: '+ str(amazonCount))
     else:
-        # print("Worked!")
-        # print("\nResult Found Below:\n")
-        # print(result)
         break
 
 if(amazonCount == 51):
     print("Error occurred gathering information from Amazon..")
 else:
-    getAmazonInformation(result)
+    theLink, thePrice = getAmazonInformation(result, links)
+    print("\n\nFound the cheapest price to be $" + thePrice)
+    print('\n')
+    print("This product can be found at: ")
+    print(theLink)
+    print('-------------------------------------------------')
+
+
+ebayCount = 0
+while ((ebayCount < 51)):
+    ebayResult, ebayLinks = searchEbay(product)
+# searchEbay(product)
+    if(ebayResult == 0):
+        ebayCount = ebayCount + 1
+        print('attempted: '+ str(ebayCount))
+    else:
+        break
+
+if(ebayCount == 51):
+    print("Error occurred gathering information from eBay..")
+else:
+    theLink, thePrice = getEbayInformation(ebayResult, ebayLinks)
+    print("\n\nFound the cheapest price to be $" + thePrice)
+    print('\n')
+    print("This product can be found at: ")
+    print(theLink)
+    print('-------------------------------------------------')
 
 
 # print("outside of the statement")
